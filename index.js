9ad3f8be94beec7919a494ef8da7712535a6c473
risha700 <ahbox@outlook.com>
2021-06-11 20:32:27 +0400

    fix path in plugin iconFolderPath

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const fs = require('fs');
 
 
 module.exports = (api, options) => {
-
     const wrote_file = path.resolve(path.join(__dirname, 'generator/iconFolderName'))
     const IconFolderPath = fs.readFileSync(wrote_file, { encoding: 'utf-8' })
     const dir_path = `${api.resolve('src/assets')}/${IconFolderPath}`
     const { configSVGIcon } = require(api.resolve('svg-icon.config.js'))
 
-    const write_file = path.resolve(path.join(__dirname, './icon_viewer/iconFolderPath'))
+    const write_file = path.resolve(path.join(__dirname, 'icon_viewer/iconFolderPath'))
     fs.writeFileSync(write_file, dir_path, {flag: 'w+'}, err => {}) 
-
     api.chainWebpack(webpackConfig => {
         configSVGIcon(webpackConfig, dir_path)
       })
